e5f0223d87cac460a7b476e9df3c5801269375b9
ToreB <toreb@mesan.no>
2015-05-12 17:06:26 +0200

    opprettet ios prosjekt

---
+++ ios_client/MesanQuiz/SpillQuizViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SpillQuizViewController.swift
+//  MesanQuiz
+//
+//  Created by Tore Brandtzæg on 04.05.15.
+//  Copyright (c) 2015 Mesan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
