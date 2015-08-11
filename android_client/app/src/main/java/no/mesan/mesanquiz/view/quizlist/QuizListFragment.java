a380ab9c55bb1703f972dcf9d52ed25991bf79d2
Thomas Pettersen <tpettersen@gmail.com>
2015-08-11 17:37:18 +0200

    initial

---
+++ android_client/app/src/main/java/no/mesan/mesanquiz/view/quizlist/QuizListFragment.java
@@ -0,0 +1,14 @@
+package no.mesan.mesanquiz.view.quizlist;
+
+import no.mesan.mesanquiz.R;
+import no.mesan.mesanquiz.view.AbstractFragment;
+
+/**
+ * Created by Thomas on 11.08.2015.
+ */
+public class QuizListFragment  extends AbstractFragment {
+    @Override
+    protected int getViewId() {
+        return R.layout.fragment_quizlist;
+    }
+}
